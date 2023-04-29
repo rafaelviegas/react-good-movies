0bfdcd1bce2197075c6efc4db8cccf10514a61e2
Rafael Viegas <rafael.viegas@arpagus.com.br>
2023-04-28 21:11:21 -0300

    feat: add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import { MainMenu } from "./MainMenu";
+import { HeaderContainer } from '../styles/HeaderContainer';
+
+export const Header = () => (
+    <HeaderContainer>
+        <h1>Good Movies</h1>
+        <MainMenu/>
+    </HeaderContainer>
+);
